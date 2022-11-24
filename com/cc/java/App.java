5381661b2b179f20e237ae1dbefe897168ee9cbe
Daniel <schindlerdaniel@web.de>
2022-11-24 13:15:30 +0100

    15min

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,14 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        Mitarbeiter kollege1 =  new Mitarbeiter("Peter", "Müller", "Professor", 1993);
+        Mitarbeiter kollege2 =  new Mitarbeiter("Hannelore", "Svenson", "StudentIn", 2021);
+        Mitarbeiter kollege3 =  new Mitarbeiter("David", "Brecht", "Assistenz", 2010);
+
+        output(kollege2.getInfo("#eintrittsjahr"));
+        output(kollege1.getInfo("#vorname"));
+        output(kollege3.getInfo("asdf"));
+
     }
 
     public static void output(String outputStr) {
--- com/cc/java/Mitarbeiter.java
+++ com/cc/java/Mitarbeiter.java
@@ -2,4 +2,38 @@ package com.cc.java;
 
 public class Mitarbeiter {
     
+    private String name;
+    private String vorname;
+    private String funktion;
+    private int eintrittsjahr;
+
+
+
+    public Mitarbeiter(String name, String vorname, String funktion, int eintrittsjahr) {
+        this.name = name;
+        this.vorname = vorname;
+        this.funktion = funktion;
+        this.eintrittsjahr = eintrittsjahr;
+    }
+
+
+
+public String getInfo(String flag){
+    switch (flag) {
+        case "#name":
+            return name;
+
+        case "#vorname":
+            return vorname;
+
+        case "#funktion":
+            return funktion;
+
+        case "#eintrittsjahr":
+            return Integer.toString(eintrittsjahr);
+    
+        default:
+            return "§$%&/ ERROR!";
+    }
+}
 }
